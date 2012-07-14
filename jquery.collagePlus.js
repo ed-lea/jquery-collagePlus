236617e9cb95dbb16e793969111507bf3f894ac5
Ed Lea <edleadesign@gmail.com>
2012-07-14 13:54:58 +0100

    adding correct min version

--- jquery.collagePlus.js
+++ jquery.collagePlus.js
@@ -4,6 +4,9 @@
  * https://github.com/ed-lea/jquery-collagePlus
  *
  * Copyright 2012, Ed Lea twitter.com/ed_lea
+ *
+ * built for http://qiip.me
+ *
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.opensource.org/licenses/GPL-2.0
